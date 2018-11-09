c4872fdd9e832763bc1a9e4da9540b1dfc78942c
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-09 14:09:49 -0700

    Added /test route to double check the pushes to heroku are working

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send({hi: 'Hello World!'});
 });
 
+app.get('/test', (req, res) => {
+  res.send({message: 'Its ok Ryan, its working now!'});
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App is listening on port: ${PORT}`);
