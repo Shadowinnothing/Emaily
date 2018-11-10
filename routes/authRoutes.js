5495de9c20a9293450e9f5d90ef486142ba60366
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-10 15:54:07 -0700

    Restructured the server

---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+  app.get('/', (req, res) => {
+    res.send({hi: 'this is just a test'});
+  });
+
+  app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }));
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+};
