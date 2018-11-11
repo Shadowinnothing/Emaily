adf154179e7e803a7dbb1504cec6a9dfbdebee7c
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-11 12:33:45 -0700

    Added some logic to prevent passport.js from saving 2 instances of the same googleID

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,16 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
-    new User({googleId: profile.id}).save(); // <- save modal instance to DB
-    //done();
+    // prevents mongo from saving more than 1 instance of a user
+    User.findOne({googleId: profile.id})
+      .then((existingUser) => {
+        if(existingUser){
+          // user already exists
+        } else {
+          // save new user to DB
+          new User({googleId: profile.id}).save(); // <- save modal instance to DB
+        }
+      });
+    //done(); // <- not sure if needed yet...
   })
 );
