b8a95a2d663da938615ec5d5cabbb0ba10f17a79
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-11 13:29:43 -0700

    Wired up cookie-session to tell express to use cookies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const keys = require('./config/keys');
 require('./models/User'); // <- have to require the schema before its used in passport.js
@@ -10,9 +12,14 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
   .catch((err) => console.log('[index.js] Deprication warning connecting to MongoDB (this is ok, its on mongoDB)'));
 
-// create app and tell it to use the routes on authRoutes.js
 const app = express();
-require('./routes/authRoutes')(app);
+app.use(cookieSession({ // <- express doesnt use cookies out the box, this tells it to use cookies
+  maxAge: 30 * 24 * 60 * 60 * 1000, // <- let cookie last for 30 days in ms
+  keys: [keys.cookieKey] // <- encrypt cookies with custom encryption
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/authRoutes')(app); // <- tell app to use these routes
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
