778a3bf0dd7c01d5bc8d5da34469afef5f3a3b24
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-11 13:56:43 -0700

    Added api/logout to logout the user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,6 +11,12 @@ module.exports = (app) => {
 
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  // logout user
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
   // returns user that is currently logged in
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
