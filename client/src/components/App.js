ec461f61ae861e1da8a4d34ae359a0ce2d4c6c9c
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-12 23:31:09 -0700

    Added BrowserRouting inside App.js and added materialize-css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import Header from './Header';
+
+const Dashboard = () => (<h2>Dashboard</h2>);
+const SurveyNew = () => (<h2>SurveyNew</h2>);
+const Landing = () => (<h2>Landing</h2>);
 
 const App = () => {
   return (
-    <p>hifdsafd!</p>
-  )
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" component={Landing} exact />
+          <Route path="/surveys" component={Dashboard} exact />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
