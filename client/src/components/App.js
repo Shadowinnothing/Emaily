88f92f0408515fe8e0f5fcd5e3f7ca5bec3e0de9
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-15 19:57:25 -0700

    Added redux-thunk and got to work on creating the fetchUser action to return current_user data from the express server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 
@@ -7,19 +9,28 @@ const Dashboard = () => (<h2>Dashboard</h2>);
 const SurveyNew = () => (<h2>SurveyNew</h2>);
 const Landing = () => (<h2>Landing</h2>);
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route path="/" component={Landing} exact />
-          <Route path="/surveys" component={Dashboard} exact />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+    // props comes from the connect method in the export default statement
+    // connect()() turns the actions into props
+    this.props.fetchUser();
+  };
+
+  render(){
+    return (
+      <div className="container"> {/* <- container Adds spacing on the left and right sides of the app */}
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/" component={Landing} exact />
+            <Route path="/surveys" component={Dashboard} exact />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  };
 };
 
-export default App;
+// connect()() turns the actions into props
+export default connect(null, actions)(App);
