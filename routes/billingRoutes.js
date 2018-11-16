5d99b5eb390d20f78442a7404482807ae05cdd32
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-16 16:27:43 -0700

    Started to really set up /api/stripe route

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,8 +4,15 @@ const stripe = require('stripe')(
 );
 
 module.exports = (app) => {
-  // recieves token for stripe and
-  app.post('/api/stripe', (req, res) => {
-     
+  // recieves token for stripe and create a charge
+  app.post('/api/stripe', async (req, res) => {
+     const charge = await stripe.charges.create({
+       amount: 500, // <- charge in pennies
+       currency: 'usd',
+       description: '$5 for 5 credits',
+       source: req.body.id
+     });
+
+     console.log(charge);
   });
 };
