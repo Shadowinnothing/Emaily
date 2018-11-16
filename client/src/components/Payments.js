017d992e5eb3a798b393c613b6faa0d2261ec0c3
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-16 14:06:58 -0700

    Added some styling to the Payments component in the Header

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,18 +5,26 @@ import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component {
 
-  /* amount is $ we want user to pay, need to specify USD in pennies
-  token expects callback that is called after we connect to the stripe api */
+  /*
+  amount is $ we want user to pay, need to specify USD in pennies
+  token expects callback that is called after we connect to the stripe api
+  */
   render() {
 
     return (
       <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
         amount={500}
         token={(token) => {
           console.log('Token:', token);
         }}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+      <button className="btn">
+        Add Credits
+      </button>
+    </StripeCheckout>
     );
   };
 };
