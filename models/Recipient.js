0be4a79bd13ebeff46464f7b104db5c9e2e31b01
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-17 12:52:51 -0700

    Added the Recipient subdocument model

---
+++ models/Recipient.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const recipientSchema({
+  email: String,
+  responded: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = recipientSchema;
