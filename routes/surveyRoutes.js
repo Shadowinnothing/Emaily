d43a92c71f7c73404daa2fda2dbeac2bb5dd2f68
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-18 12:32:56 -0700

    Tweaked the /api/surveys route and added SendGrid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,23 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+// dont require the surveySchema.js file to avoid problems
+// with importing surveySchema all over the place
+const Survey = mongoose.model('surveys');
+
 module.exports = (app) => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const {title, subject, body, recipients} = req.body;
 
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      // splits emails up and returns an array of objects
+      recipients: recipients.split(',').map((email) => ({email: email.trim()})),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
   });
-
 };
