f388ed1f980208bcd63ad997576c9878c6f5bf86
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-18 16:23:44 -0700

    Tested the Mailer and It works now

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,10 @@ import 'materialize-css/dist/css/materialize.min.css';
 import App from './components/App';
 import reducers from './reducers'; // <- will look for index.js file by default
 
+// temperary to test emailing
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
--- services/Mailer.js
+++ services/Mailer.js
@@ -45,6 +45,19 @@ class Mailer extends helper.Mail {
     this.addPersonalization(personalize);
   };
 
+  // send Mailer class off to sendgrid to send the
+  // emails to all recipients
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = this.sgApi.API(request);
+    return response;
+  };
+
 };
 
 module.exports = Mailer;
