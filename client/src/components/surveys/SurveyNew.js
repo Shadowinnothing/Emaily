edf353939ebc76df5f4855677d39e2fa7f00397f
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-20 20:27:39 -0700

    Added boiler for SurveyFormReview to let the user double check if their info is correct

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
 
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 // renders survey form
 class SurveyNew extends Component {
 
+  // create-react-app lets us initialize state like this
+  state = {showFormReview: false};
+
+  renderContent() {
+    if(this.state.showFormReview) {
+      return <SurveyFormReview />;
+    };
+    return <SurveyForm
+      onSurveySubmit={() => this.setState({showFormReview: true})}
+    />;
+  };
+
   render() {
     return (
       <div>
-        <SurveyForm />
+        {this.renderContent()}
       </div>
     );
   };
