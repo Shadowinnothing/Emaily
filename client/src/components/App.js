820dd0f626604ff2db807ca568a91c43f7a635b8
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-21 18:27:06 -0700

    Wired up localtunnel for our webhook. Also added a ThankYouPage component so clicking yes or no in the email doesnt send the user to the 404 page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,6 +8,7 @@ import Landing from './Landing';
 import Dashboard from './Dashboard';
 import SurveyNew from './surveys/SurveyNew';
 import NotFoundPage from './NotFoundPage';
+import ThankYouPage from './surveys/ThankYouPage';
 
 class App extends Component {
   componentDidMount(){
@@ -25,6 +26,7 @@ class App extends Component {
             <Route path="/" component={Landing} exact />
             <Route path="/surveys" component={Dashboard} exact />
             <Route path="/surveys/new" component={SurveyNew} />
+            <Route path="/api/surveys/thanks" component={ThankYouPage} />
             <Route component={NotFoundPage} />
           </Switch>
         </div>
---
+++ client/src/components/surveys/ThankYouPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ThankYouPage = () => {
+  return (
+    <div>
+      <h3>Thank you for your feedback!</h3>
+    </div>
+  );
+};
+
+export default ThankYouPage;
